4f424cefc2c1b92bc0aab87b62d7a379caafe442
TuckerLandis <landistuckerc@gmail.com>
2021-08-08 14:54:43 -0500

    UserProfileService

--- src/main/java/com/tucknology/springawsapi/profile/UserProfileService.java
+++ src/main/java/com/tucknology/springawsapi/profile/UserProfileService.java
@@ -1,4 +1,19 @@
 package com.tucknology.springawsapi.profile;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class UserProfileService {
+
+    private final UserProfileDataAccessService userProfileDataAccessService;
+
+    @Autowired
+    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService){
+        this.userProfileDataAccessService = userProfileDataAccessService;
+    }
+
+    List<UserProfile> getUserProfiles(){
+        return  userProfileDataAccessService.getUserProfiles();
+    }
 }
