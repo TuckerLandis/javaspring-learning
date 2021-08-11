00571d92e60df8cd86181c6132970635ec140961
TuckerLandis <landistuckerc@gmail.com>
2021-08-10 22:28:11 -0500

    server logic, checking if image, checking if empty

--- src/main/java/com/tucknology/springawsapi/profile/UserProfileService.java
+++ src/main/java/com/tucknology/springawsapi/profile/UserProfileService.java
@@ -1,11 +1,15 @@
 package com.tucknology.springawsapi.profile;
 
+import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+
+import static org.apache.http.entity.ContentType.*;
 
 @Service
 public class UserProfileService {
@@ -23,7 +27,11 @@ public class UserProfileService {
 
     void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
         //1. check if image is not empty
+        if (file.isEmpty()) {
+            throw new IllegalStateException("Cannot upload empty file" + file.getSize());
+        }
         //2. if file is image
+        if (!Arrays.asList(IMAGE_JPEG, IMAGE_PNG, IMAGE_GIF).contains(file.getContentType())
         //3. user exists
         //4. grab metadata
         //5. store image in s3, then update database (userProfileImageLink)
