425c6a1f5ac977acd6caee8d1773ad0bcab45306
TuckerLandis <landistuckerc@gmail.com>
2021-08-10 21:40:21 -0500

    dropzone

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import React, {useState, useEffect, useCallback} from 'react';
+import {useDropzone} from 'react-dropzone'
 import './App.css';
+import axios from "axios";
+
+const UserProfiles = () => {
+
+  const [userProfiles, setUserProfiles] = useState([])
+
+  const fetchUserProfiles = () => {
+    axios.get('http://localhost:8080/api/v1/user-profile/')
+    .then(res => {
+      console.log(res);
+      setUserProfiles(res.data)
+
+      
+    })
+  }
+  
+    useEffect(() => {
+      fetchUserProfiles();
+    }, [])
+
+    return userProfiles.map((userProfile, index) => {
+
+      return(
+        <div key={index}>
+          <Dropzone />
+          <h1>
+            {userProfile.username}
+          </h1>
+          <p>
+            {userProfile.userProfileId}
+          </p>
+        </div>
+      )
+    })
+  
+}
+
+
+
+function Dropzone() {
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+
+    const file = acceptedFiles[0];
+    
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
+  )
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+    <UserProfiles />
+
     </div>
   );
 }
